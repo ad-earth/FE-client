f11ac0e0f4420eb2e8ba185ad3c0e02002a5b7fa
sol <osunpine@gmail.com>
2022-08-24 07:12:27 +0900

    📝 feat. global theme 추가

---
+++ src/style/theme.ts
@@ -0,0 +1,43 @@
+export const theme = {
+  fontSize: {
+    xsmall: "10px",
+    small: "12px",
+    medium: "16px",
+    large: "22px",
+  },
+  colors: {
+    green1: "rgba(0,94,38,1)",
+    green2: "#00913A",
+    green3: "#00993A",
+    green4: "#d5ead9",
+    gray1: "#646464",
+    gray2: "#999999",
+    gray3: "#E5E7EB",
+    gray4: "#f6f6f6",
+    black: "#212121",
+    yellow: "#ffda03",
+    red: "#ff462d",
+  },
+  radius: {
+    round: "30px",
+    circle: "50%",
+  },
+  badgeBackground: {
+    new: "#00913a",
+    best: "#fff",
+    soldout: "#666",
+    sale: "#fe5356",
+  },
+  badgeColor: {
+    new: "#fff",
+    best: "#fb4637",
+    soldout: "#fff",
+    sale: "#fff",
+  },
+  badgeBorder: {
+    new: "1px solid #00913a",
+    best: "1px solid #ccc",
+    soldout: "1px solid #666",
+    sale: "1px solid #fe5356",
+  },
+};
