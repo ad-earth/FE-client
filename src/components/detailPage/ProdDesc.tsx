20a8c9b2ee4b7f6b9b8c94b86efc68941ea4cde7
sol <osunpine@gmail.com>
2022-08-25 23:47:27 +0900

    📂 docs. ProdName.tsx & ProdDesc.tsx 파일 생성

---
+++ src/components/detailPage/ProdDesc.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProdDesc = () => {
+  return <div></div>;
+};
+
+export default ProdDesc;
---
+++ src/components/detailPage/ProdName.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProdName = () => {
+  return <div></div>;
+};
+
+export default ProdName;
