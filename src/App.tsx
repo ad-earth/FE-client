dfae45d169d12e78b5feeecc96c605a7aa89925d
sol <osunpine@gmail.com>
2022-08-26 03:24:16 +0900

    🧹 chore. ThemeProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import Header from "./containers/Header";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -12,25 +13,28 @@ import DetailPage from "./pages/DetailPage";
 import PaymentPage from "./pages/PaymentPage";
 import CompletePage from "./pages/CompletePage";
 import Footer from "./containers/Footer";
+import { theme } from "./style/theme";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/login" element={<LogInPage />}></Route>
-        <Route path="/signup" element={<SignUpPage />}></Route>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/mypage" element={<MyPage />}></Route>
-        <Route path="/cart" element={<CartPage />}></Route>
-        <Route path="/list" element={<ListPage />}></Route>
-        <Route path="/search" element={<SearchPage />}></Route>
-        <Route path="/detail" element={<DetailPage />}></Route>
-        <Route path="/payment" element={<PaymentPage />}></Route>
-        <Route path="/complete" element={<CompletePage />}></Route>
-      </Routes>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/login" element={<LogInPage />}></Route>
+          <Route path="/signup" element={<SignUpPage />}></Route>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/mypage" element={<MyPage />}></Route>
+          <Route path="/cart" element={<CartPage />}></Route>
+          <Route path="/list" element={<ListPage />}></Route>
+          <Route path="/search" element={<SearchPage />}></Route>
+          <Route path="/detail" element={<DetailPage />}></Route>
+          <Route path="/payment" element={<PaymentPage />}></Route>
+          <Route path="/complete" element={<CompletePage />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
