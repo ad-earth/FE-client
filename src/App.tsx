fe6c63c2907a9c886f50f8c027d44274410d3828
whl5105 <whl5105@naver.com>
2022-08-25 20:39:42 +0900

    📂  docs. mypage  container 파일 추가
    - containers > mypage 파일푸가
    - router-dom mypage outlet 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./containers/Header";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
 import MainPage from "./pages/MainPage";
-import MyPage from "./pages/MyPage";
 import CartPage from "./pages/CartPage";
 import ListPage from "./pages/ListPage";
 import DetailPage from "./pages/DetailPage";
@@ -14,21 +13,33 @@ import PaymentPage from "./pages/PaymentPage";
 import CompletePage from "./pages/CompletePage";
 import Footer from "./containers/Footer";
 
+//mypage
+import Mypage from "./pages/MyPage";
+import Order from "./containers/mypage/Order";
+import Wish from "./containers/mypage/Wish";
+import Cancel from "./containers/mypage/Cancel";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/login" element={<LogInPage />}></Route>
-        <Route path="/signup" element={<SignUpPage />}></Route>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/mypage" element={<MyPage />}></Route>
-        <Route path="/cart" element={<CartPage />}></Route>
-        <Route path="/list" element={<ListPage />}></Route>
-        <Route path="/detail" element={<DetailPage />}></Route>
-        <Route path="/payment" element={<PaymentPage />}></Route>
-        <Route path="/complete" element={<CompletePage />}></Route>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LogInPage />}></Route>
+          <Route path="/signup" element={<SignUpPage />}></Route>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/mypage/*" element={<Mypage />}>
+            <Route path="" element={<Order />} />
+            <Route path="wish" element={<Wish />} />
+            <Route path="cancel" element={<Cancel />} />
+          </Route>
+          <Route path="/cart" element={<CartPage />}></Route>
+          <Route path="/list" element={<ListPage />}></Route>
+          <Route path="/detail" element={<DetailPage />}></Route>
+          <Route path="/payment" element={<PaymentPage />}></Route>
+          <Route path="/complete" element={<CompletePage />}></Route>
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
