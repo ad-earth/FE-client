fb13ad2a1e3acad12b173a2d04a45adc36d05b36
sol <osunpine@gmail.com>
2022-08-26 22:16:40 +0900

    📝 feat. 메인 소셜 링크 view

--- src/containers/mainPage/SocialLinkList.tsx
+++ src/containers/mainPage/SocialLinkList.tsx
@@ -3,13 +3,17 @@ import styled from "styled-components";
 const SocialLinkList = () => {
   return (
     <LinkBg>
-      <ImgWrapper>
+      <Link>
         <img src="https://cdn.imweb.me/thumbnail/20220629/de27d3749e375.png" />
-      </ImgWrapper>
-      <ImgWrapper>
-        <img src="https://cdn.imweb.me/thumbnail/20220303/b7aa3459058d0.png" />
-        <img src="https://cdn.imweb.me/thumbnail/20220622/37b2485c495d2.jpg" />
-      </ImgWrapper>
+      </Link>
+      <LinkWrapper>
+        <LinkS>
+          <img src="https://cdn.imweb.me/thumbnail/20220303/b7aa3459058d0.png" />
+        </LinkS>
+        <LinkS>
+          <img src="https://cdn.imweb.me/thumbnail/20220622/37b2485c495d2.jpg" />
+        </LinkS>
+      </LinkWrapper>
     </LinkBg>
   );
 };
@@ -20,9 +24,35 @@ const LinkBg = styled.div`
   width: 100%;
   background: #ffffe9;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  margin: 50px auto;
+  padding: 50px 0;
 `;
-const ImgWrapper = styled.div`
+const Link = styled.div`
+  display: flex;
+  width: 40%;
+  overflow: hidden;
+  border-radius: 20px;
+  img {
+    width: 100%;
+  }
+`;
+const LinkWrapper = styled.div`
   width: 40%;
   display: flex;
   flex-direction: column;
+  gap: 30px;
+`;
+const LinkS = styled.div`
+  display: flex;
+  width: 100%;
+  overflow: hidden;
+  border-radius: 20px;
+  cursor: pointer;
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
 `;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,23 @@
-const MainPage: React.FC = () => {
-  return null;
+import styled from "styled-components";
+import BestProdList from "../containers/mainPage/BestProdList";
+import MainBanner from "../containers/mainPage/MainBanner";
+import SocialLinkList from "../containers/mainPage/SocialLinkList";
+
+const MainPage = () => {
+  return (
+    <>
+      <MainBanner />
+      <MainContainer>
+        <BestProdList />
+        <SocialLinkList />
+      </MainContainer>
+    </>
+  );
 };
+
 export default MainPage;
+
+const MainContainer = styled.div`
+  /* width: 1200px; */
+  margin: 0 auto;
+`;
