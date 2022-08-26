8f5835ef9a60677fa0af80eb87588d07ee8baecd
whl5105 <whl5105@naver.com>
2022-08-27 03:11:17 +0900

    📝  feat.  구매평 리뷰 모달  뷰 작업

--- src/components/modal/ReviewModal.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ReviewModal = () => {
-  return <div>ReviewModal</div>;
-};
-
-export default ReviewModal;
--- src/containers/mypage/Order.tsx
+++ src/containers/mypage/Order.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useState } from "react";
+import ReviewModal from "../../components/modal/reaviewModal/ReviewModal";
 
 const Order = () => {
-  return <div>Order</div>;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <div>
+      Order
+      <button onClick={() => setIsOpen(true)}>모달 열기</button>
+      <ReviewModal isOpen={true} handleClose={() => setIsOpen(false)} />
+    </div>
+  );
 };
 
 export default Order;
