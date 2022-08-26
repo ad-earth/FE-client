a0a070b60f5791f8ef5351a4777f0f9203bf1700
sol <osunpine@gmail.com>
2022-08-26 14:54:16 +0900

    📝 feat. main banner carousel

--- src/containers/mainPage/MainBanner.tsx
+++ src/containers/mainPage/MainBanner.tsx
@@ -1,7 +1,44 @@
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./mainBanner.css";
 
 const MainBanner = () => {
-  return <div></div>;
+  // carousel option
+  const setting = {
+    autoplaySpeed: 3000,
+    autoplay: true,
+    dotsClass: "dotsCustom",
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: (dots: React.ReactNode) => (
+      <DotsWrapper>
+        <ul>{dots}</ul>
+      </DotsWrapper>
+    ),
+  };
+  return (
+    <Slider {...setting}>
+      <BannerImg src="/img/b1.jpeg" />
+      <BannerImg src="/img/b2.jpeg" />
+      <BannerImg src="/img/b3.png" />
+    </Slider>
+  );
 };
 
 export default MainBanner;
+
+const BannerImg = styled.img`
+  width: 100%;
+  cursor: pointer;
+`;
+const DotsWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+`;
