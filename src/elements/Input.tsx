44b6de8df2339e79018a9074ac93f4a439976e4a
sol <osunpine@gmail.com>
2022-08-28 00:35:28 +0900

    🧹 chore. 코드 정리

--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -1,26 +1,25 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 interface InputType {
-  type?: string
-  outline?: string
-  fontSize?: string
-  width?: string
-  marginTop?: string
-  padding?: string
+  type?: string;
+  outline?: string;
+  fontSize?: string;
+  width?: string;
+  marginTop?: string;
+  padding?: string;
 }
 
 export const Input = () => {
-  return <MyInput></MyInput>
-}
+  return <MyInput></MyInput>;
+};
 
 const MyInput = styled.input<InputType>`
-  type: ${(props) => (props.type ? props.type : 'text')};
-  outline: ${(props) => (props.outline ? props.outline : 'none')};
-  font-size: ${(props) => (props.fontSize ? props.fontSize : 'inherit')};
-  width: ${(props) => (props.width ? props.width : '100%')};
+  outline: ${(props) => (props.outline ? props.outline : "none")};
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "inherit")};
+  width: ${(props) => (props.width ? props.width : "100%")};
   border: 1px solid #e5e7eb;
-  margin-top: ${(props) => (props.marginTop ? props.marginTop : '-1px')};
-  padding: ${(props) => (props.padding ? props.padding : '8px 16px')};
+  margin-top: ${(props) => (props.marginTop ? props.marginTop : "-1px")};
+  padding: ${(props) => (props.padding ? props.padding : "8px 16px")};
   box-sizing: border-box;
-  font-wieght: 400;
-`
+  font-weight: 400;
+`;
