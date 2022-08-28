9712cc87d183c8bbdd2c234f84f678f289dc07de
sol <osunpine@gmail.com>
2022-08-28 15:05:55 +0900

    📂 docs. 메인 페이지 containers 생성

---
+++ src/containers/mainPage/NewProdList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewProdList = () => {
+  return <div></div>;
+};
+
+export default NewProdList;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,18 @@
-const MainPage: React.FC = () => {
-  return null;
+import MainBanner from "../containers/mainPage/MainBanner";
+import BestProdList from "../containers/mainPage/BestProdList";
+import SocialLinkList from "../containers/mainPage/SocialLinkList";
+import NewProdList from "../containers/mainPage/NewProdList";
+import ShopGoal from "../containers/mainPage/ShopGoal";
+
+const MainPage = () => {
+  return (
+    <>
+      <MainBanner />
+      <BestProdList />
+      <SocialLinkList />
+      <NewProdList />
+      <ShopGoal />
+    </>
+  );
 };
 export default MainPage;
