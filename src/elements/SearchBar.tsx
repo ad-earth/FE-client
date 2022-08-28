498fef10f4253fecfa4b36379f056f9c73a6788a
damiiya <ddam2lee@gmail.com>
2022-08-29 06:14:32 +0900

    📝 feat. SearchBar view 작업

--- src/elements/SearchBar.tsx
+++ src/elements/SearchBar.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
 import styled from 'styled-components'
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
-interface InputType {
-    children?: React.ReactNode;
-}
-
-export const SearchBar = (props: InputType) => {
+export const SearchBar = () => {
   return (
-    <InputDiv><SearchInput/><SearchRoundedIcon/></InputDiv>
+    <InputDiv>
+      <SearchInput placeholder='Search'/>
+      <SearchIcon/>
+    </InputDiv>
   )
 }
 
-const InputDiv = styled.div<InputType>`
+const InputDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 240px;
+  height: 34px;
+  padding: 0 20px;
+  margin: 25px 30px 10px 30px;
+  border: 1px solid ${({theme}) => theme.colors.gray3};
+  border-radius: ${({theme}) => theme.radius.round};
 `
-
-const SearchInput = styled.div<InputType>`
+const SearchInput = styled.input`
+  width: 180px;
+  padding: 0 10px 0 0;
+  border: none;
+  :focus {
+    outline: none;
+  }
+  ::placeholder {
+    color: ${({theme}) => theme.colors.gray2};
+    font-size: 14px;
+  }
 `
+const SearchIcon = styled(SearchRoundedIcon)({
+  color: '#999',
+})
