95d7061fb5f48b7b5d15ef6df6ffa9998ec95f98
whl5105 <whl5105@naver.com>
2022-08-30 03:21:23 +0900

    📝 feat. 마이페이지 스타일 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,16 +1,32 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+//components
 import Aside from "../containers/myPage/Aside";
 import UserHead from "../containers/myPage/UserHead";
 
 const MyPage = () => {
   return (
-    <div className="Wrap">
+    <Wrap className="Wrap">
       <Aside />
-      <>
+      <Section>
         <UserHead />
         <Outlet />
-      </>
-    </div>
+      </Section>
+    </Wrap>
   );
 };
 export default MyPage;
+
+const Wrap = styled.div`
+  max-width: 1200px;
+  background-color: red;
+  margin: 80px auto 100px;
+  padding: 0 20px;
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+`;
+const Section = styled.div`
+  width: 84%;
+  background: blue;
+`;
