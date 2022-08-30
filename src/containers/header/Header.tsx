d063621c6c8591edadaac0aa9605fd47f980cf13
damiiya <ddam2lee@gmail.com>
2022-08-31 06:36:57 +0900

    📝 feat. 헤더 아래 border 수정

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -13,36 +13,38 @@ const Header = () => {
 
   return (
     <t.HeaderContainer>
-      <t.LeftDiv>
-        <t.LogoDiv>
-          <t.LogoImg src={headLogo} />
-        </t.LogoDiv>
-        <t.CategorySpan>장보기</t.CategorySpan>
-      </t.LeftDiv>
-      <t.RightDiv>
-        <SearchBar />
-        {LoginState ? (
-          <>
-            <t.LogInSpan>로그인</t.LogInSpan>
-            <t.LogOutSpan>회원가입</t.LogOutSpan>
-            <t.IconDiv>
-              <t.ShopIcon sx={{ fontSize: 32 }} />
-            </t.IconDiv>
-          </>
-        ) : (
-          <>
-            <t.LogInSpan>로그아웃</t.LogInSpan>
-            <t.IconDiv>
-              <t.UserIcon sx={{ fontSize: 32, cursor: "pointer" }} />
-            </t.IconDiv>
-            <t.IconDiv>
-              <t.CountBadge badgeContent={1}>
-                <t.ShopIcon sx={{ fontSize: 32, cursor: "pointer" }} />
-              </t.CountBadge>
-            </t.IconDiv>
-          </>
-        )}
-      </t.RightDiv>
+      <t.HeaderWrapper>
+        <t.LeftDiv>
+          <t.LogoDiv>
+            <t.LogoImg src={headLogo} />
+          </t.LogoDiv>
+          <t.CategorySpan>장보기</t.CategorySpan>
+        </t.LeftDiv>
+        <t.RightDiv>
+          <SearchBar />
+          {LoginState ? (
+            <>
+              <t.LogInSpan>로그인</t.LogInSpan>
+              <t.LogOutSpan>회원가입</t.LogOutSpan>
+              <t.IconDiv>
+                <t.ShopIcon sx={{ fontSize: 32 }} />
+              </t.IconDiv>
+            </>
+          ) : (
+            <>
+              <t.LogInSpan>로그아웃</t.LogInSpan>
+              <t.IconDiv>
+                <t.UserIcon sx={{ fontSize: 32, cursor: "pointer" }} />
+              </t.IconDiv>
+              <t.IconDiv>
+                <t.CountBadge badgeContent={1}>
+                  <t.ShopIcon sx={{ fontSize: 32, cursor: "pointer" }} />
+                </t.CountBadge>
+              </t.IconDiv>
+            </>
+          )}
+        </t.RightDiv>
+      </t.HeaderWrapper>
     </t.HeaderContainer>
   );
 };
