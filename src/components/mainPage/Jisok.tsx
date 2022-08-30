faefd694bd076277e2a6fe1aa659e409181b533a
sol <osunpine@gmail.com>
2022-08-30 12:49:36 +0900

    📂 docs. 파일 생성

---
+++ src/components/mainPage/Jisok.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Jisok = () => {
+  return <div></div>;
+};
+
+export default Jisok;
---
+++ src/components/mainPage/MissionBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MissionBox = () => {
+  return <div></div>;
+};
+
+export default MissionBox;
