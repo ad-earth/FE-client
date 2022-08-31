e4a1c5b059c933df6bedae6247614f1bd0b40a8e
hyooyh8910 <hyooyh8910@naver.com>
2022-08-31 20:21:22 +0900

    📂 docs. paymentpage 컨테이너 안 파일 생성

---
+++ src/containers/paymentPage/OrderPList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OrderPList = () => {
+  return <div>OrderPList</div>
+}
+
+export default OrderPList
---
+++ src/containers/paymentPage/OrderPay.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OrderPay = () => {
+  return <div>OrderPay</div>
+}
+
+export default OrderPay
