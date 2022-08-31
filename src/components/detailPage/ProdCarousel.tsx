389e1915c67a2ca470c37abbfa120ee1428b71d6
sol <osunpine@gmail.com>
2022-08-31 19:50:40 +0900

    📝 feat. 상세페이지 썸네일 반응형 구현

--- src/components/detailPage/ProdCarousel.tsx
+++ src/components/detailPage/ProdCarousel.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProdCarousel = () => {
-  return <div></div>;
+  // carousel option
+  const setting = {
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <MainContainer>
+      <Slider {...setting}>
+        <ProdImg src="https://cdn.imweb.me/thumbnail/20220816/ed7c65d28916d.jpg" />
+        <ProdImg src="https://cdn.imweb.me/thumbnail/20220816/1ff8ce0d9cc08.jpg" />
+        <ProdImg src="https://cdn.imweb.me/thumbnail/20220331/6a2e9b4face67.jpg" />
+      </Slider>
+      <Page>1 / 3</Page>
+    </MainContainer>
+  );
 };
 
 export default ProdCarousel;
+
+const MainContainer = styled.div`
+  position: relative;
+  width: 98%;
+  margin: 0 auto;
+`;
+const ProdImg = styled.img`
+  width: 100%;
+  object-fit: contain;
+`;
+const Page = styled.div`
+  position: absolute;
+  width: 40px;
+  padding: 3px 8px;
+  box-sizing: border-box;
+  background: #646464;
+  color: #fff;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  left: 20px;
+  bottom: 20px;
+`;
--- src/containers/ProdInfo.tsx
+++ src/containers/ProdInfo.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+import ProdCarousel from "../components/detailPage/ProdCarousel";
 import ProdImg from "../components/detailPage/ProdImg";
 import ProdName from "../components/detailPage/ProdName";
 import ProdDesc from "../components/detailPage/ProdDesc";
 import ProdOpt from "../components/detailPage/ProdOpt";
 
 const ProdInfo = () => {
+  //뷰포트 사이즈에 따라 상품 썸네일 carousel로 변경
+  const [viewport, setViewport] = useState(visualViewport.width);
+  useEffect(() => {
+    const resizeListener = () => {
+      setViewport(visualViewport.width);
+    };
+    window.addEventListener("resize", resizeListener);
+  });
+
   return (
     <ProdInfoContainer>
-      <ProdImg />
+      {viewport <= 990 ? <ProdCarousel /> : <ProdImg />}
       <InfoWrapper>
         <ProdName />
         <ProdDesc />
@@ -22,12 +32,30 @@ export default ProdInfo;
 
 const ProdInfoContainer = styled.div`
   width: 1200px;
-  margin: 0 auto;
+  margin: 15px auto;
+  padding: 15px;
+  box-sizing: border-box;
   display: flex;
   justify-content: space-between;
+  @media (min-width: 991px) and (max-width: 1200px) {
+    width: 100%;
+  }
+  @media (max-width: 990px) {
+    width: 100%;
+    padding: 0px;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 const InfoWrapper = styled.div`
   width: 41%;
   display: flex;
   flex-direction: column;
+  padding: 15px;
+  box-sizing: border-box;
+  @media (max-width: 990px) {
+    padding: 0px;
+    width: 98%;
+    margin-top: 20px;
+  }
 `;
