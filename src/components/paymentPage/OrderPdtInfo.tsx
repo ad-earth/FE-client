bb60e7168a3468f3dc81eb748a18e17d08be1f9a
hyooyh8910 <hyooyh8910@naver.com>
2022-09-02 03:56:02 +0900

    📝 feat. 결제 페이지 배송비 css 추가

---
+++ src/components/paymentPage/OrderPdtInfo.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+export const FreeShipping = () => {
+  return (
+    <>
+      <ShippingFee>배송비 0원</ShippingFee>
+    </>
+  )
+}
+
+const ShippingFee = styled.div`
+  background: rgba(33, 33, 33, 0.03);
+  text-align: center;
+  margin-bottom: 10px;
+  padding: 10px 14px;
+  border: 1px solid rgba(33, 33, 33, 0.15);
+  border-top: none;
+  font-size: 13px;
+  line-height: 1.4;
+`
