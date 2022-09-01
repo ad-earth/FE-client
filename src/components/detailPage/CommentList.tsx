172f8e885e154b75debbd35316587c7d63ac514e
sol <osunpine@gmail.com>
2022-09-01 10:30:35 +0900

    📂 docs. 파일 생성

---
+++ src/components/detailPage/CommentList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CommentList = () => {
+  return <div></div>;
+};
+
+export default CommentList;
---
+++ src/components/detailPage/CommentPreview.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CommentPreview = () => {
+  return <div></div>;
+};
+
+export default CommentPreview;
