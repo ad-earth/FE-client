d58e408870a2f110e3b205e4208ffa11fe029605
hyooyh8910 <hyooyh8910@naver.com>
2022-09-02 04:01:23 +0900

    📝 feat. 결제 페이지 왼쪽 css 뷰 추가

--- src/containers/paymentPage/OrderPList.tsx
+++ src/containers/paymentPage/OrderPList.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import styled from 'styled-components'
+import { FreeShipping } from '../../components/paymentPage/OrderPdtInfo'
+import PdtInfo from '../../components/paymentPage/PdtInfo'
+import PayMethod from '../../components/paymentPage/PayMethod'
+import PayUserInfo from '../../components/paymentPage/PayUserInfo'
 
 const OrderPList = () => {
-  return <div>OrderPList</div>
+  const titles = ['주문 상품 정보', '주문자 정보', '배송 정보']
+  return (
+    <>
+      <PListArea>
+        <TipOff>
+          <OrderInfoDiv>{titles[0]}</OrderInfoDiv>
+          <PdtInfo />
+          <PdtInfo />
+          <FreeShipping />
+        </TipOff>
+        <TipOff>
+          <OrderInfoDiv>{titles[1]}</OrderInfoDiv>
+          <PayUserInfo />
+        </TipOff>
+        <TipOff>
+          <OrderInfoDiv>{titles[2]}</OrderInfoDiv>
+          <PayMethod />
+        </TipOff>
+      </PListArea>
+    </>
+  )
 }
 
 export default OrderPList
+
+const PListArea = styled.div`
+  width: 470px;
+  visibility: visible;
+  float: left;
+  display: flex;
+  background-color: yellow;
+  flex-direction: column;
+`
+const TipOff = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 24px;
+  margin-bottom: 16px;
+  background-color: #fff;
+  flex-direction: column;
+`
+export const OrderInfoDiv = styled.div`
+  font-size: inherit;
+  font-weight: bold;
+  padding-bottom: 20px;
+}
+`
