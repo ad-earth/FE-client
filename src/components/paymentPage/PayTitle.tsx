9dec79ba3739f6cd3ef79f16202a97bf5d1dbabd
hyooyh8910 <hyooyh8910@naver.com>
2022-09-02 13:05:30 +0900

    📝 feat. 결제페이지 타이틀 div css 추가

--- src/components/paymentPage/PayTitle.tsx
+++ src/components/paymentPage/PayTitle.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import styled from 'styled-components'
 
 const PayTitle = () => {
-  return <div>PayTitle</div>
+  return <H1tag>결제하기</H1tag>
 }
 
 export default PayTitle
+
+const H1tag = styled.h1`
+  font-size: 28px;
+  color: #20252b;
+  text-align: center;
+  margin: 60px 0;
+  font-weight: bold;
+`
