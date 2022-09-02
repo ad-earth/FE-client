6614d66b90ee364fc1c8040873974cd000861f06
hyooyh8910 <hyooyh8910@naver.com>
2022-09-02 15:06:09 +0900

    📝 feat. 결제페이지 오른편 css 추가

--- src/containers/paymentPage/OrderPay.tsx
+++ src/containers/paymentPage/OrderPay.tsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+import PayRadioBtn from '../../components/paymentPage/PayRadioBtn'
+import PaySummary from '../../components/paymentPage/PaySummary'
+import { OrderInfoDiv } from './OrderPList'
+import PayAgree from '../../components/paymentPage/PayAgree'
 
 const OrderPay = () => {
-  return <div>OrderPay</div>
+  const titles = ['주문 요약', '결제수단']
+  return (
+    <PayArea>
+      <TipOff>
+        <OrderInfoDiv>{titles[0]}</OrderInfoDiv>
+        <PaySummary />
+      </TipOff>
+      <TipOff>
+        <OrderInfoDiv>{titles[1]}</OrderInfoDiv>
+        <PayRadioBtn></PayRadioBtn>
+      </TipOff>
+      <TipOff>
+        <PayAgree />
+      </TipOff>
+    </PayArea>
+  )
 }
 
 export default OrderPay
+
+const PayArea = styled.div`
+  margin-left: 16px;
+  width: 450px;
+  visibility: visible;
+  float: right;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  align-items: flex-end;
+`
+const TipOff = styled.div`
+  width: 79%;
+  display: flex;
+  padding: 24px;
+  margin-bottom: 16px;
+  background-color: #fff;
+  flex-direction: column;
+`
