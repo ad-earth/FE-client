5696b86fb4e292724524321e0383f60a1292cfed
hyooyh8910 <hyooyh8910@naver.com>
2022-09-02 20:29:51 +0900

    📝 feat. 검색페이지 카테고리버튼 네브바 css 추가

--- src/components/searchPage/SearchCateDrop.tsx
+++ src/components/searchPage/SearchCateDrop.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import styled from 'styled-components'
 
 const SearchCateDrop = () => {
-  return <div>SearchCateDrop</div>
+  return (
+    <>
+      <Grid>
+        <Text>63개의 검색결과</Text>
+        <BtnArea style={{ gridColumn: '2/2', gridRow: '1' }}>
+          <DropBtn name="select" id="select" defaultValue={'first'}>
+            <option value="first">최신순</option>
+          </DropBtn>
+        </BtnArea>
+      </Grid>
+    </>
+  )
 }
 
 export default SearchCateDrop
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 88% 12%;
+  margin: 20px 0 40px;
+`
+
+const BtnArea = styled.div`
+  margin: 0px 20px 25px;
+`
+const DropBtn = styled.select`
+  width: 80px;
+  margin-left: 20px;
+  font-size: 15px;
+  cursor: pointer;
+  color: #646464;
+  border: none;
+  transition: box-shadow 0.4s ease;
+  outline-color: #fff;
+`
+const Text = styled.div`
+  font-size: 15px;
+  color: #7a7a7a;
+  margin-left: 20px;
+`
--- src/components/searchPage/SearchNav.tsx
+++ src/components/searchPage/SearchNav.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import styled from 'styled-components'
 
 const SearchNav = () => {
-  return <div>SearchNav</div>
+  return (
+    <>
+      <LineDiv>
+        <span>검색결과</span>
+      </LineDiv>
+    </>
+  )
 }
-
 export default SearchNav
+
+const LineDiv = styled.div`
+  display: flex;
+  width: 97%;
+  border-width: 0 0 1px;
+  border-style: solid;
+  border-color: rgba(33, 33, 33, 0.15);
+  margin: 0px auto;
+  padding-bottom: 15px;
+  & span {
+    font-size: 15px;
+    color: #646464;
+  }
+`
