63dc8652347ddb6320c4a4afa03e2622e287ac77
hyooyh8910 <hyooyh8910@naver.com>
2022-09-02 16:11:28 +0900

    📝 feat.주문완료Page title css 추가

--- src/containers/completePage/CompleteTitle.tsx
+++ src/containers/completePage/CompleteTitle.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import styled from 'styled-components'
 
 const CompleteTitle = () => {
-  return <div>CompleteTitle</div>
+  const completeText = '아래 계좌정보로 입금해 주시면\n 결제 완료처리가 됩니다'
+  return (
+    <>
+      <Div>
+        <H1tag>주문완료</H1tag>
+        <H6tag>{completeText}</H6tag>
+      </Div>
+    </>
+  )
 }
 
 export default CompleteTitle
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const H1tag = styled.h1`
+  font-size: 26px;
+  color: #20252b;
+  text-align: center;
+  margin: 10px 0 0px;
+  padding-top: 50px;
+  font-weight: normal;
+`
+const H6tag = styled.h6`
+  font-size: 20px;
+  color: #757575;
+  text-align: center;
+  margin: 10px 0 10px;
+  font-weight: bold;
+  white-space: pre-wrap;
+`
