d505eac243c5d77c50008b60df4cc95c54287344
hyooyh8910 <hyooyh8910@naver.com>
2022-09-02 15:05:30 +0900

    📝 feat. 결제페이지 page css 추가

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,4 +1,30 @@
-const PaymentPage: React.FC = () => {
-  return null;
-};
-export default PaymentPage;
+import styled from 'styled-components'
+import PayTitle from '../components/paymentPage/PayTitle'
+import OrderPay from '../containers/paymentPage/OrderPay'
+import OrderPList from '../containers/paymentPage/OrderPList'
+
+const PaymentPage = () => {
+  return (
+    <>
+      <Area>
+        <PayTitle />
+        <PayArea>
+          <OrderPList />
+          <OrderPay></OrderPay>
+        </PayArea>
+      </Area>
+    </>
+  )
+}
+export default PaymentPage
+const Area = styled.div`
+  background-color: #f3f3f3;
+  display: grid;
+  justify-content: center;
+  width: 100%;
+`
+
+const PayArea = styled.div`
+  margin: 0px auto;
+  margin-bottom: 100px;
+`
