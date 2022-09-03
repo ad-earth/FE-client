e0cb1e41c9d79b79b38ffbbca4c9262864d79ff1
hyooyh8910 <hyooyh8910@naver.com>
2022-09-04 00:29:55 +0900

    📂 docs. 결제 페이지 배송정보변경 탭 파일생성

---
+++ src/components/paymentPage/PayMethodInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PayMethodInput = () => {
+  return <div>PayMethodInput</div>
+}
+
+export default PayMethodInput
--- src/components/paymentPage/PayUserInpu.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const PayUserInpu = () => {
-  return <div>PayUserInpu</div>
-}
-
-export default PayUserInpu
