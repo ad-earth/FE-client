6c60ff35f41e00372321ee30601b2c73de60c7b2
damiiya <ddam2lee@gmail.com>
2022-09-06 04:28:12 +0900

    📝 feat. 검색 모달 (반응형: 990px) 작업 완료

--- src/components/modal/schProdModal/SchProdModal.tsx
+++ src/components/modal/schProdModal/SchProdModal.tsx
@@ -1,9 +1,6 @@
-import styled from "styled-components";
+import * as t from "./SchProdModal.style";
 import { theme } from "../../../style/theme";
 import Modal from "../Modal";
-
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 interface ModalType {
   isOpen?: boolean;
   handleClose: () => void;
@@ -14,13 +11,13 @@ const SchProdModal = (props: ModalType) => {
     <>
       {props.isOpen && (
         <Modal handleClose={() => props.handleClose()} isOpen={props.isOpen}>
-          <SchContainer>
-            <Close onClick={() => props.handleClose()} />
-            <SchInput>
+          <t.SchContainer>
+            <t.Close onClick={() => props.handleClose()} />
+            <t.SchInput>
               <input type="text" placeholder="검색" />
-              <SchIcon />
-            </SchInput>
-          </SchContainer>
+              <t.SchIcon />
+            </t.SchInput>
+          </t.SchContainer>
         </Modal>
       )}
     </>
@@ -28,51 +25,3 @@ const SchProdModal = (props: ModalType) => {
 };
 
 export default SchProdModal;
-
-const SchContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  margin-top: 400px;
-`;
-const Close = styled(CloseRoundedIcon)({
-  "&.MuiSvgIcon-root": {
-    fontSize: `${theme.fs40}`,
-    color: `${theme.ls16}`,
-    position: "absolute",
-    top: 15,
-    right: 15,
-    cursor: "pointer",
-  },
-});
-const SchInput = styled.div`
-  width: 60%;
-  height: 5%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-bottom: 1px solid ${theme.ls16};
-  input {
-    width: 90%;
-    height: 100%;
-    border: none;
-    font-size: ${theme.fs24};
-    :focus {
-      outline: none;
-      ::placeholder {
-        color: transparent;
-      }
-    }
-    ::placeholder {
-      color: ${theme.fc15};
-    }
-  }
-`;
-const SchIcon = styled(SearchRoundedIcon)({
-  "&.MuiSvgIcon-root": {
-    fontSize: `${theme.fs40}`,
-    color: `${theme.fc15}`,
-    cursor: "pointer",
-  },
-});
