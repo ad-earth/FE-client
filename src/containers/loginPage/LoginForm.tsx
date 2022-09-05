5f04c6c183c0d971e4df5cc4834a4b6463a38b67
damiiya <ddam2lee@gmail.com>
2022-09-06 04:29:16 +0900

    📝 feat. 로그인 페이지 작업 완료

--- src/containers/loginPage/LoginForm.tsx
+++ src/containers/loginPage/LoginForm.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import * as t from "./LoginForm.style";
+import { useState } from "react";
+
+import SearchModal from "../../components/modal/searchModal/schModal/SearchModal";
+import { Input } from "../../elements/Input";
+import { MainButton } from "../../elements/Buttons";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [searchIsOpen, setSearchIsOpen] = useState<boolean>(false);
+  return (
+    <>
+      <SearchModal
+        isOpen={searchIsOpen}
+        handleClose={() => setSearchIsOpen(false)}
+      />
+      <t.LogInWrapper>
+        <Input holderName={"아이디"} />
+        <Input holderName={"비밀번호"} />
+        <t.CheckWrapper>
+          <t.LogInCheckbox type="checkbox" />
+          <t.CheckLabel>로그인 상태유지</t.CheckLabel>
+        </t.CheckWrapper>
+        <MainButton radius={"30px"} fontSize={"14px"}>
+          {"로그인"}
+        </MainButton>
+        <t.AddWrapper>
+          <t.AddInfo>회원가입</t.AddInfo>
+          <t.AddInfo onClick={() => setSearchIsOpen(true)}>
+            아이디 ∙ 비밀번호 찾기
+          </t.AddInfo>
+        </t.AddWrapper>
+      </t.LogInWrapper>
+    </>
+  );
 };
 
 export default LoginForm;
