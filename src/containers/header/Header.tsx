efc0c9a778e5d4d540b76e79bae5cdb521089b6f
damiiya <ddam2lee@gmail.com>
2022-09-06 01:04:21 +0900

    📝 feat. Header 반응형 작업 & 추가 컨테이너 작업 진행중

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,51 +1,85 @@
-import { useState } from "react";
-import * as t from "./HeaderStyle";
+import * as t from "./Header.style";
+import { useEffect, useState } from "react";
 
 import { SearchBar } from "./../../elements/SearchBar";
 import headLogo from "./../../assets/logo/headLogo.png";
-
-interface PropsType {
-  LoginState: boolean;
-}
+import SchProdModal from "../../components/modal/schProdModal/SchProdModal";
+import ScrollHeader from "../../components/header/scroll/ScrollHeader";
 
 const Header = () => {
   const [LoginState, setLoginState] = useState(false);
+  const [viewport, setViewport] = useState(visualViewport.width);
+  const [searchIsOpen, setSearchIsOpen] = useState<boolean>(false);
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
+
+  useEffect(() => {
+    const resizeListener = () => {
+      setViewport(visualViewport.width);
+    };
+    window.addEventListener("resize", resizeListener);
+  });
 
   return (
-    <t.HeaderContainer>
-      <t.HeaderWrapper>
-        <t.LeftDiv>
-          <t.LogoDiv>
-            <t.LogoImg src={headLogo} />
-          </t.LogoDiv>
-          <t.CategorySpan>장보기</t.CategorySpan>
-        </t.LeftDiv>
-        <t.RightDiv>
-          <SearchBar />
-          {LoginState ? (
-            <>
-              <t.LogInSpan>로그인</t.LogInSpan>
-              <t.LogOutSpan>회원가입</t.LogOutSpan>
-              <t.IconDiv>
-                <t.ShopIcon sx={{ fontSize: 32 }} />
-              </t.IconDiv>
-            </>
-          ) : (
-            <>
-              <t.LogInSpan>로그아웃</t.LogInSpan>
-              <t.IconDiv>
-                <t.UserIcon sx={{ fontSize: 32, cursor: "pointer" }} />
-              </t.IconDiv>
-              <t.IconDiv>
-                <t.CountBadge badgeContent={1}>
-                  <t.ShopIcon sx={{ fontSize: 32, cursor: "pointer" }} />
-                </t.CountBadge>
-              </t.IconDiv>
-            </>
-          )}
-        </t.RightDiv>
-      </t.HeaderWrapper>
-    </t.HeaderContainer>
+    <>
+      <SchProdModal
+        isOpen={searchIsOpen}
+        handleClose={() => setSearchIsOpen(false)}
+      />
+      <t.HeaderContainer>
+        <t.HeaderWrapper>
+          <t.LeftDiv>
+            <img src={headLogo} />
+            <p>장보기</p>
+          </t.LeftDiv>
+          <t.RightDiv>
+            <SearchBar />
+            {LoginState ? (
+              <>
+                {viewport >= 990 ? (
+                  <>
+                    <span>로그인</span>
+                    <span>회원가입</span>
+                    <t.ShopIcon />
+                  </>
+                ) : (
+                  <>
+                    <t.ShopIcon />
+                    <t.SchIcon onClick={() => setSearchIsOpen(true)} />
+                    <t.EtcIcon onClick={() => setInfoIsOpen(true)} />
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                {viewport >= 990 ? (
+                  <>
+                    <span>로그아웃</span>
+                    <t.UserIcon />
+                    <div>
+                      <t.CountBadge badgeContent={1}>
+                        <t.ShopIcon />
+                      </t.CountBadge>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div>
+                      <t.CountBadge badgeContent={1}>
+                        <t.ShopIcon />
+                      </t.CountBadge>
+                    </div>
+                    <t.UserIcon />
+                    <t.SchIcon onClick={() => setSearchIsOpen(true)} />
+                    <t.EtcIcon onClick={() => setInfoIsOpen(true)} />
+                  </>
+                )}
+              </>
+            )}
+          </t.RightDiv>
+        </t.HeaderWrapper>
+      </t.HeaderContainer>
+      <ScrollHeader />
+    </>
   );
 };
 
