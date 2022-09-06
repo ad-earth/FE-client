d7d020526a3727dd83b6089d90137ce2f496b809
hyooyh8910 <hyooyh8910@naver.com>
2022-09-07 00:05:30 +0900

    📝 feat.주문확인페이지 컨테이너 파일명 및 경로변경

---
+++ src/containers/completePage/completeTitle/CompleteTitle.tsx
@@ -0,0 +1,15 @@
+import * as t from './CompleteTitle.style'
+
+const CompleteTitle = () => {
+  const completeText = '아래 계좌정보로 입금해 주시면\n 결제 완료처리가 됩니다'
+  return (
+    <>
+      <t.Div>
+        <t.H1tag>주문완료</t.H1tag>
+        <t.H6tag>{completeText}</t.H6tag>
+      </t.Div>
+    </>
+  )
+}
+
+export default CompleteTitle
