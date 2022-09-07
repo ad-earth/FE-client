4e5407a72034995cb1e634d25ffa088664a77ab7
sol <osunpine@gmail.com>
2022-09-07 23:06:07 +0900

    📝 feat. react-query & devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+//query
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import Header from "./containers/header/Header";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -24,36 +28,41 @@ import CancelDetail from "./containers/myPage/CancelDetail";
 import { theme } from "./style/theme";
 import GlobalStyle from "./style/GlobalStyle";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/login" element={<LogInPage />}></Route>
-          <Route path="/signup" element={<SignUpPage />}></Route>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/mypage" element={<Mypage />}>
-            <Route index element={<Order />} />
-            <Route path=":id" element={<OrderDetail />} />
-            <Route path="wish" element={<Wish />} />
-            <Route path="cancel-call/:id" element={<CancelDetail />} />
-            <Route path="cancel">
-              <Route index element={<Cancel />} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/login" element={<LogInPage />}></Route>
+            <Route path="/signup" element={<SignUpPage />}></Route>
+            <Route path="/" element={<MainPage />}></Route>
+            <Route path="/mypage" element={<Mypage />}>
+              <Route index element={<Order />} />
               <Route path=":id" element={<OrderDetail />} />
+              <Route path="wish" element={<Wish />} />
+              <Route path="cancel-call/:id" element={<CancelDetail />} />
+              <Route path="cancel">
+                <Route index element={<Cancel />} />
+                <Route path=":id" element={<OrderDetail />} />
+              </Route>
             </Route>
-          </Route>
-          <Route path="/cart" element={<CartPage />}></Route>
-          <Route path="/list" element={<ListPage />}></Route>
-          <Route path="/search" element={<SearchPage />}></Route>
-          <Route path="/detail/:prodNo" element={<DetailPage />}></Route>
-          <Route path="/payment" element={<PaymentPage />}></Route>
-          <Route path="/complete" element={<CompletePage />}></Route>
-        </Routes>
-        <Footer />
-      </div>
-    </ThemeProvider>
+            <Route path="/cart" element={<CartPage />}></Route>
+            <Route path="/list" element={<ListPage />}></Route>
+            <Route path="/search" element={<SearchPage />}></Route>
+            <Route path="/detail/:prodNo" element={<DetailPage />}></Route>
+            <Route path="/payment" element={<PaymentPage />}></Route>
+            <Route path="/complete" element={<CompletePage />}></Route>
+          </Routes>
+          <Footer />
+          <ReactQueryDevtools />
+        </div>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
