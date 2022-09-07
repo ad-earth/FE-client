663ab069d3237469960d0435cf18bf5a59909e8e
sol <osunpine@gmail.com>
2022-09-07 23:07:25 +0900

    📝 feat. main best 상품 타입

---
+++ src/components/mainPage/bestCards/bestCards.type.ts
@@ -0,0 +1,12 @@
+export interface BestType {
+  a_Brand: string;
+  p_Cost: number;
+  p_Discount: number;
+  p_Name: string;
+  p_No: number;
+  p_Best: boolean;
+  p_New: boolean;
+  p_Sale: boolean;
+  p_Soldout: boolean;
+  p_Thumbnail: string[];
+}
