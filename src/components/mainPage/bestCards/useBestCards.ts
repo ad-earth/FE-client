72d5b246a6c64b26de884fe457fdc5943d3a248f
sol <osunpine@gmail.com>
2022-09-09 11:31:23 +0900

    📝 feat. 공통 타입으로 변경 적용

--- src/components/mainPage/bestCards/useBestCards.ts
+++ src/components/mainPage/bestCards/useBestCards.ts
@@ -1,14 +1,14 @@
 import { useQuery } from "react-query";
 
 import { getMain } from "../../../shared/apis/api";
-import { BestType } from "./bestCards.type";
+import { CardType } from "../../../shared/types/types";
 
-async function getBestCards(): Promise<BestType[]> {
+async function getBestCards(): Promise<CardType[]> {
   const { data } = await getMain();
   return data.Best;
 }
 
-export function useBestCards(): BestType[] {
+export function useBestCards(): CardType[] {
   const fallback: [] = [];
   const { data = fallback } = useQuery("best", getBestCards);
   return data;
