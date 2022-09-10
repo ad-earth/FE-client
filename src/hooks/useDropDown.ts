2e890bd662d8582214f5dd56387220e0a75efb68
damiiya <ddam2lee@gmail.com>
2022-09-11 06:39:43 +0900

    📝 feat. 드롭다운 커스텀 훅 작성

---
+++ src/hooks/useDropDown.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState, useRef } from "react";
+
+const useDropDown = () => {
+  const [dropIsOpen, setDropIsOpen] = useState<boolean>(false);
+  const dropRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  const removeHandler = () => {
+    setDropIsOpen(!dropIsOpen);
+  };
+
+  useEffect(() => {
+    function handleDrop(event: React.BaseSyntheticEvent | MouseEvent) {
+      if (dropRef.current && !dropRef.current.contains(event.target)) {
+        setDropIsOpen(!dropIsOpen);
+      }
+    }
+
+    document.addEventListener("click", handleDrop, true);
+
+    return () => {
+      document.removeEventListener("click", handleDrop, true);
+    };
+  }, [dropRef]);
+
+  return {
+    isDropped: dropIsOpen,
+    dropRef: dropRef,
+    handleRemove: removeHandler,
+  };
+};
+
+export default useDropDown;
