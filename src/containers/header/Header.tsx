6fd1cc4051e6347f7f0ddfd0926dd91a053ad9a5
damiiya <ddam2lee@gmail.com>
2022-09-11 10:35:59 +0900

    📝 feat. 헤더 코드 수정

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -5,12 +5,31 @@ import { SearchBar } from "./../../elements/SearchBar";
 import headLogo from "./../../assets/logo/headLogo.png";
 import SchProdModal from "../../components/modal/schProdModal/SchProdModal";
 import ScrollHeader from "../../components/header/scroll/ScrollHeader";
+import MenuDrop from "../../elements/MenuDrop";
+import useDropDown from "../../hooks/useDropDown";
+import AsideHeader from "../../components/header/aside/AsideHeader";
+
+let data: {
+  id: number;
+  cate: string;
+  path: string;
+}[] = [
+  { id: 1, cate: "전체", path: "" },
+  { id: 2, cate: "욕실", path: "" },
+  { id: 3, cate: "주방", path: "" },
+  { id: 4, cate: "음료용품", path: "" },
+  { id: 5, cate: "생활", path: "" },
+  { id: 6, cate: "식품", path: "" },
+  { id: 7, cate: "화장품", path: "" },
+  { id: 8, cate: "문구", path: "" },
+];
 
 const Header = () => {
   const [LoginState, setLoginState] = useState(false);
   const [viewport, setViewport] = useState(visualViewport.width);
   const [searchIsOpen, setSearchIsOpen] = useState<boolean>(false);
   const [infoIsOpen, setInfoIsOpen] = useState(false);
+  const { isDropped, dropRef, handleRemove } = useDropDown();
 
   useEffect(() => {
     const resizeListener = () => {
@@ -25,11 +44,16 @@ const Header = () => {
         isOpen={searchIsOpen}
         handleClose={() => setSearchIsOpen(false)}
       />
-      <t.HeaderContainer>
+      <AsideHeader
+        isOpen={infoIsOpen}
+        handleClose={() => setInfoIsOpen(false)}
+      />
+      <t.HeaderContainer ref={dropRef}>
         <t.HeaderWrapper>
           <t.LeftDiv>
             <img src={headLogo} />
-            <p>장보기</p>
+            <p onClick={handleRemove}>장보기</p>
+            <MenuDrop isDropped={isDropped} data={data} />
           </t.LeftDiv>
           <t.RightDiv>
             <SearchBar />
