167968c8cbdc695118ecd96a009c0bb70d95ab8f
sol <osunpine@gmail.com>
2022-09-20 18:38:50 +0900

    📝 feat. 상단 page navigation 작업 (#84)

--- src/components/detailPage/history/History.tsx
+++ src/components/detailPage/history/History.tsx
@@ -1,10 +1,38 @@
+// import { useState } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 import * as t from "./history.style";
 
 const History = () => {
+  const navigate = useNavigate();
+  const { category } = useParams();
+
   return (
     <t.HistoryNav>
-      <span>Home &nbsp; &gt; </span>&nbsp;<span>장보기 &nbsp;&gt;</span>
-      &nbsp;<div>식품</div>
+      <span
+        onClick={() => {
+          navigate(`/`);
+        }}
+      >
+        Home &nbsp; &gt;{" "}
+      </span>
+      &nbsp;
+      <span
+        onClick={() => {
+          navigate(`/list`);
+        }}
+      >
+        장보기 &nbsp;&gt;
+      </span>
+      &nbsp;
+      <div
+        onClick={() => {
+          navigate(category === "전체" ? `/list` : `/list/${category}`);
+        }}
+      >
+        {category}
+      </div>
     </t.HistoryNav>
   );
 };
