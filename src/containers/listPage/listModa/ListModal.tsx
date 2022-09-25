5f7d79c235813e72ea3d1587434aaab7c6aad448
sol <osunpine@gmail.com>
2022-09-26 02:48:09 +0900

    🧹 chore. 변수명 변경으로 인한 에러 코드 임시 주석 처리

--- src/containers/listPage/listModa/ListModal.tsx
+++ src/containers/listPage/listModa/ListModal.tsx
@@ -1,50 +1,46 @@
-import * as t from './ListModal.style'
-import { useEffect, useState } from 'react'
+import * as t from "./ListModal.style";
+import { useEffect, useState } from "react";
 
-import Modal from '../../../components/modal/Modal'
-import ProdImg from '../../../components/detailPage/prodImg/ProdImg'
-import ProdName from '../../../components/detailPage/prodName/ProdName'
-import ProdDesc from '../../../components/detailPage/prodDesc/ProdDesc'
-import ProdOpt from '../../../components/detailPage/prodOpt/ProdOpt'
-import ProdCarousel from '../../../components/detailPage/prodCarousel/ProdCarousel'
+import Modal from "../../../components/modal/Modal";
+import ProdImg from "../../../components/detailPage/prodImg/ProdImg";
+import ProdName from "../../../components/detailPage/prodName/ProdName";
+import ProdDesc from "../../../components/detailPage/prodDesc/ProdDesc";
+import ProdOpt from "../../../components/detailPage/prodOpt/ProdOpt";
+import ProdCarousel from "../../../components/detailPage/prodCarousel/ProdCarousel";
 
 interface ModalType {
-  isOpen: boolean
-  handleClose: () => void
+  isOpen: boolean;
+  handleClose: () => void;
 }
 const ListModal = (props: ModalType) => {
   function close(e: React.MouseEvent) {
-    e.preventDefault()
-    props.handleClose()
+    e.preventDefault();
+    props.handleClose();
   }
 
-  const [viewport, setViewport] = useState(visualViewport.width)
+  const [viewport, setViewport] = useState(visualViewport.width);
   useEffect(() => {
     const resizeListener = () => {
-      setViewport(visualViewport.width)
-    }
-    window.addEventListener('resize', resizeListener)
-  })
+      setViewport(visualViewport.width);
+    };
+    window.addEventListener("resize", resizeListener);
+  });
 
   return (
     <>
       {props.isOpen && (
         <Modal handleClose={() => props.handleClose()} isOpen={props.isOpen}>
           <t.BtnDiv>
-            <t.Xbtn sx={{ fontSize: '36px' }} onClick={close} />
+            <t.Xbtn sx={{ fontSize: "36px" }} onClick={close} />
             <t.ProdInfoContainer>
               {viewport <= 990 ? (
-                <t.Max>
-                  <ProdCarousel />
-                </t.Max>
+                <t.Max>{/* <ProdCarousel /> */}</t.Max>
               ) : (
                 <ProdImg />
               )}
               <t.InfoWrapper>
-                <ProdName />
-                <t.CaroDiv>
-                  <ProdDesc />
-                </t.CaroDiv>
+                {/* <ProdName /> */}
+                <t.CaroDiv>{/* <ProdDesc /> */}</t.CaroDiv>
                 <ProdOpt />
               </t.InfoWrapper>
             </t.ProdInfoContainer>
@@ -52,7 +48,7 @@ const ListModal = (props: ModalType) => {
         </Modal>
       )}
     </>
-  )
-}
+  );
+};
 
-export default ListModal
+export default ListModal;
