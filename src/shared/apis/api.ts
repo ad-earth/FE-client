de3efd043561aade1c4c2b6fc6a9af5d3d45ccdc
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-09-25 23:14:46 +0900

    📝 feat. mypage API 수정

--- src/shared/apis/api.ts
+++ src/shared/apis/api.ts
@@ -5,18 +5,31 @@ export const getOrders = (page: number) =>
   axiosInstance.get(`/orders?page=${page}&maxpost=10`);
 export const getOrderDetail = (id: string) =>
   axiosInstance.get(`/orders/${id}`);
-export const putCancel = (id: string, body: number[]) =>
-  axiosInstance.put(`/orders/${id}/cancel`, { body });
 export const getCancel = (page: number) =>
   axiosInstance.get(`/cancel-list?page=${page}&maxpost=10`);
+export const putCancel = (id: string, body: number[]) =>
+  axiosInstance.put(`/orders/${id}/cancel`, { body });
 export const getWishList = (page: number) =>
   axiosInstance.get(`/wish-list?page=${page}&maxpost=12`);
-export const putUserInfoChange = () => axiosInstance.put(`/users`);
+export const putUserInfoChange = (
+  u_Name: string,
+  u_Address1: string,
+  u_Address2: string,
+  u_Gender: string,
+  u_Phone: string,
+  u_Img: string | null
+) =>
+  axiosInstance.put(`/users`, {
+    u_Name,
+    u_Address1,
+    u_Address2,
+    u_Gender,
+    u_Phone,
+    u_Img,
+  });
+export const postReviews = (id: number, r_Content: string, r_Score: number) =>
+  axiosInstance.post(`/reviews/${id}`, { r_Content, r_Score });
 export const deleteUserWithdrawal = () => axiosInstance.delete(`/users`);
-export const postReviews = (id: number, body: any) =>
-  axiosInstance.get(`/reviews/?${id}`);
-// export const postReviews = (id: number, body: object) =>
-//   axiosInstance.post(`/reviews/:${id}`);
 
 //메인페이지
 export const getMain = () => axiosInstance.get(`/main`);
