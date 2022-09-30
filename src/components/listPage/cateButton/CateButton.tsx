2d00650c817826f09e2977ff606d35c443fa6e34
hyooyh8910 <hyooyh8910@naver.com>
2022-09-30 18:01:19 +0900

    📝 feat. 결제페이지 기능 추가

--- src/components/listPage/cateButton/CateButton.tsx
+++ src/components/listPage/cateButton/CateButton.tsx
@@ -2,10 +2,12 @@ import * as t from "./CateButton.style";
 
 const CateButton = ({
   // c,
+  // param,
   cate,
   cateClick,
 }: {
   // c: (e: any) => void;
+  // param: string;
   cate: string;
   cateClick: (e: any) => void;
 }) => {
@@ -24,35 +26,22 @@ const CateButton = ({
     <>
       <t.BtnArea>
         <t.BtnWrap className="desktop">
-          <t.CBtn value="전체" onClick={cateClick}>
-            전체
-          </t.CBtn>
-          <t.CBtn value="욕실" onClick={cateClick}>
-            욕실
-          </t.CBtn>
-          <t.CBtn value="주방" onClick={cateClick}>
-            주방
-          </t.CBtn>
-          <t.CBtn value="음료용품" onClick={cateClick}>
-            음료용품
-          </t.CBtn>
-          <t.CBtn value="생활" onClick={cateClick}>
-            생활
-          </t.CBtn>
-          <t.CBtn value="식품" onClick={cateClick}>
-            식품
-          </t.CBtn>
-          <t.CBtn value="화장품" onClick={cateClick}>
-            화장품
-          </t.CBtn>
-          <t.CBtn value="문구" onClick={cateClick}>
-            문구
-          </t.CBtn>
+          {categoryList.map((val, i) => {
+            return (
+              <t.CBtn key={i} value={val} onClick={cateClick}>
+                {val}
+              </t.CBtn>
+            );
+          })}
         </t.BtnWrap>
       </t.BtnArea>
       <t.BtnWrap className="mobile">
         {categoryList.map((val, i) => {
-          return <t.SmallCBtn key={i}>{val}</t.SmallCBtn>;
+          return (
+            <t.SmallCBtn key={i} value={val} onClick={cateClick}>
+              {val}
+            </t.SmallCBtn>
+          );
         })}
         <t.LineArea />
       </t.BtnWrap>
@@ -70,3 +59,30 @@ export default CateButton;
 //     );
 //   });
 // }
+
+// <t.BtnWrap className="desktop">
+//   <t.CBtn value="전체" onClick={cateClick}>
+//     전체
+//   </t.CBtn>
+//   <t.CBtn value="욕실" onClick={cateClick}>
+//     욕실
+//   </t.CBtn>
+//   <t.CBtn value="주방" onClick={cateClick}>
+//     주방
+//   </t.CBtn>
+//   <t.CBtn value="음료용품" onClick={cateClick}>
+//     음료용품
+//   </t.CBtn>
+//   <t.CBtn value="생활" onClick={cateClick}>
+//     생활
+//   </t.CBtn>
+//   <t.CBtn value="식품" onClick={cateClick}>
+//     식품
+//   </t.CBtn>
+//   <t.CBtn value="화장품" onClick={cateClick}>
+//     화장품
+//   </t.CBtn>
+//   <t.CBtn value="문구" onClick={cateClick}>
+//     문구
+//   </t.CBtn>
+// </t.BtnWrap>;
--- src/components/paymentPage/payRadioBtn/PayRadioBtn.tsx
+++ src/components/paymentPage/payRadioBtn/PayRadioBtn.tsx
@@ -1,91 +1,128 @@
-import * as t from './PayRadioBtn.style'
-import React, { useState } from 'react'
+import * as t from "./PayRadioBtn.style";
+import React, { useState } from "react";
 
-import { Input } from '../../../elements/Input'
-import { DropBtn } from '../payMethod/PayMethod.style'
+import { Input } from "../../../elements/Input";
+import { DropBtn } from "../payMethod/PayMethod.style";
+import { useRef } from "react";
 
 export interface RadiobtnType {
-  value?: string
-  checked?: boolean
-  type?: string
-  select?: boolean
-  top?: string
-  bgColor?: string
+  value?: string;
+  checked?: boolean;
+  type?: string;
+  select?: boolean;
+  top?: string;
+  bgColor?: string;
 }
 
 const PayRadioBtn = () => {
-  const [select, setSelect] = useState('optionA')
+  const [select, setSelect] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const numberRef = useRef<HTMLInputElement>(null);
+
+  const checkHandler = (checked: boolean) => {
+    if (checked) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  };
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-    setSelect(value)
-  }
+    const value = e.target.value;
+    numberRef.current.value = "";
+    if (value === "checked") {
+      setSelect(value);
+    } else {
+      setSelect(value);
+    }
+  };
+
   return (
     <t.Wrapper>
       <t.Item>
         <t.RadioButton
           type="radio"
           name="payment"
-          value="bankbook"
-          checked={select === 'bankbook'}
-          onChange={(e) => handleSelectChange(e)}
+          value="checked"
+          checked={true}
+          readOnly
         />
         <t.RadioButtonLabel />
         <t.Text>무통장입금</t.Text>
       </t.Item>
-      <DropBtn
-        style={{ fontSize: '14px' }}
-        name="payment"
-        id="-select"
-        defaultValue={'first'}
-      >
+      <DropBtn style={{ fontSize: "14px" }} id="-select" defaultValue={"first"}>
         <option value="first">지구은행 12309812 (주)광고지구</option>
       </DropBtn>
       <Input
         fontSize="14px"
-        holderName={'입금자명 (미입력시 주문자명)'}
+        holderName={"입금자명 (미입력시 주문자명)"}
         marginTop="10px"
+        color="#20252b"
       />
       <t.LineDiv>
         <t.PayText>주문 후 24시간동안 미입금시 자동취소됩니다.</t.PayText>
       </t.LineDiv>
 
-      <t.Item style={{ margin: '15px 0 20px' }}>
-        <t.CheckBtn type="checkbox" />
+      <t.Item style={{ margin: "15px 0 20px" }}>
+        <t.CheckBtn
+          type="checkbox"
+          value="checked"
+          onChange={(e) => {
+            checkHandler(e.target.checked);
+          }}
+        />
         <t.Text>현금영수증 신청</t.Text>
       </t.Item>
-      <div style={{ display: 'flex' }}>
-        <t.Item>
-          <t.RadioButton
-            type="radio"
-            name="paycheck"
-            value="paycheck1"
-            checked={select === 'paycheck1'}
-            onChange={(e) => handleSelectChange(e)}
-          />
-          <t.RadioButtonLabel />
-          <t.Text>소득공제용</t.Text>
-        </t.Item>
-        <t.Item style={{ marginLeft: '30px' }}>
-          <t.RadioButton
-            type="radio"
-            name="paycheck"
-            value="paycheck2"
-            checked={select === 'paycheck2'}
-            onChange={(e) => handleSelectChange(e)}
-          />
-          <t.RadioButtonLabel />
-          <t.Text>지출증빙용</t.Text>
-        </t.Item>
-      </div>
-      <Input
-        fontSize="14px"
-        holderName={'휴대전화번호 입력'}
-        marginTop="10px"
-      />
+      {open ? (
+        <>
+          <div style={{ display: "flex" }}>
+            <t.Item>
+              <t.RadioButton
+                type="radio"
+                name="paycheck"
+                value="paycheck1"
+                checked={select === "paycheck1"}
+                onChange={(e) => handleSelectChange(e)}
+              />
+              <t.RadioButtonLabel />
+              <t.Text>소득공제용</t.Text>
+            </t.Item>
+
+            <t.Item style={{ marginLeft: "30px" }}>
+              <t.RadioButton
+                type="radio"
+                name="paycheck"
+                value="paycheck2"
+                checked={select === "paycheck2"}
+                onChange={(e) => handleSelectChange(e)}
+              />
+              <t.RadioButtonLabel />
+              <t.Text>지출증빙용</t.Text>
+            </t.Item>
+          </div>
+          {select === "paycheck2" ? (
+            <Input
+              fontSize="14px"
+              holderName={"사업자번호 입력"}
+              marginTop="10px"
+              color="#20252b"
+              ref={numberRef}
+            />
+          ) : (
+            <Input
+              fontSize="14px"
+              holderName={"휴대전화번호 입력"}
+              marginTop="10px"
+              color="#20252b"
+              ref={numberRef}
+            />
+          )}
+        </>
+      ) : null}
     </t.Wrapper>
-  )
-}
+  );
+};
 
 export const RadioBtn = (props: RadiobtnType) => {
   return (
@@ -99,7 +136,7 @@ export const RadioBtn = (props: RadiobtnType) => {
         <t.RadioButtonLabel top={props.top} />
       </t.Item>
     </>
-  )
-}
+  );
+};
 
-export default PayRadioBtn
+export default PayRadioBtn;
