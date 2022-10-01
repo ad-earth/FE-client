c02397f8677cdc3761239ad68e7a62a659bab329
damiiya <ddam2lee@gmail.com>
2022-10-02 04:48:58 +0900

    📝 feat. Input 코드 수정

--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-
 import { theme } from "../style/theme";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputType {
   outline?: string;
@@ -12,13 +12,27 @@ interface InputType {
   color?: string;
   bgColor?: string;
   fBorder?: string;
+  value?: string;
+  type?: string;
+  ref?: any;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = (props: InputType) => {
+const Input: ForwardRefRenderFunction<HTMLInputElement, InputType> = (
+  { ...props },
+  ref
+) => {
   return (
-    <MyInput type="text" placeholder={props.holderName} {...props}></MyInput>
+    <MyInput
+      type={props.type}
+      placeholder={props.holderName}
+      ref={ref}
+      {...props}
+    ></MyInput>
   );
 };
+
+export default forwardRef(Input);
 
 const MyInput = styled.input<InputType>`
   outline: ${(props) => (props.outline ? props.outline : "none")};
