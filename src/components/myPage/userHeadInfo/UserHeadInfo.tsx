bb6b8e22351a9a9e863117ca774c5ebb3377f8d1
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-15 03:15:15 +0900

    📝 feat. 스토리지 유저정보 코드 추가

--- src/components/myPage/userHeadInfo/UserHeadInfo.tsx
+++ src/components/myPage/userHeadInfo/UserHeadInfo.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from "react";
 import * as t from "./userHeadInfo.style";
 import Avatar from "@mui/material/Avatar";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
+const userName = window.localStorage.getItem("u_Name");
+
 const UserHeadInfo = () => {
   const isMobile = useMediaQuery("(max-width: 990px)");
-  useEffect(() => {
-    window.localStorage.setItem("u_Name", "최수인");
-  }, []);
   return (
     <t.UserBox>
       <t.UserImg>
@@ -17,7 +15,7 @@ const UserHeadInfo = () => {
         />
       </t.UserImg>
       <t.UserName>
-        안녕하세요 <strong>최수인님</strong>
+        안녕하세요 <strong>{userName}님</strong>
         <br />
         오늘도 같이 지구를 살려봅시다 🌍
       </t.UserName>
