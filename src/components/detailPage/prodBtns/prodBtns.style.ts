f12cb41b39a71b7784830c80187906d3ad099ecb
sol <osunpine@gmail.com>
2022-10-23 05:46:56 +0900

    📂 docs. 폴더 구조 정리 (#125)

--- src/components/detailPage/prodBtns/prodBtns.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const BtnWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  gap: 8px;
-  margin-top: 25px;
-`;
