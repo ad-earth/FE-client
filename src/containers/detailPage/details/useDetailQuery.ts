ddaa250e45f02b24a968a9a34577f8c8236b8733
sol <osunpine@gmail.com>
2022-10-25 20:41:48 +0900

    🐛 fix. 에러 처리 코드 수정

--- src/containers/detailPage/details/useDetailQuery.ts
+++ src/containers/detailPage/details/useDetailQuery.ts
@@ -1,10 +1,14 @@
 import { useQuery } from "react-query";
+import { setErrorMessage, setIsError } from "../../../redux/reducer/errorSlice";
+import { useAppDispatch } from "../../../redux/store";
 import { getDetails } from "../../../shared/apis/api";
 
 export const useDetailQuery = (productNo: string, keyWord?: string) => {
+  const dispatch = useAppDispatch();
+
   return useQuery("detail", () => getDetails(productNo, keyWord), {
-    onError: ({ response }) => {
-      return response.data.errorMessage;
+    onError: ({ message }) => {
+      dispatch(setIsError(true)) && dispatch(setErrorMessage(message));
     },
   });
 };
