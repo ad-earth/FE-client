ef232ea67f16547fa1fe7772a49d5cb86f9df5b4
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-30 01:22:57 +0900

    🐛 fix.  상세페이지 리뷰모달 열결 및 코드 정리 #165

---
+++ src/components/myPage/order/orderListDetail/orderListDetail.type.ts
@@ -0,0 +1,9 @@
+export interface PropsType {
+  p_Thumbnail: string[];
+  a_Brand: string;
+  p_Name: string;
+  p_Option: Array<
+    [string | null, string | null, number | null, number | null, number | null]
+  >;
+  o_Status: string;
+}
