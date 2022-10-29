76f2b3aa72f996aa47603920f66e35a71f9e5c0e
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-29 15:52:46 +0900

    📝 feat. 반응형 asideNav 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 //components
-import Aside from "../containers/myPage/Aside";
+import { Aside } from "../containers/myPage/Aside";
 import UserHead from "../containers/myPage/UserHead";
 
 const MyPage = () => {
@@ -23,15 +23,21 @@ const Wrap = styled.div`
   position: relative;
   display: flex;
   padding: 0 20px;
+
   @media (max-width: 990px) {
     padding: 0;
+    flex-direction: column;
+    margin: 0 auto 100px;
+    background-color: ${({ theme }) => theme.bg06};
   }
 `;
 const Section = styled.div`
   width: 84%;
   box-sizing: border-box;
   padding: 0 15px;
+  background-color: ${({ theme }) => theme.bg01};
   @media (max-width: 990px) {
     width: 100%;
+    border-top: 1px solid ${({ theme }) => theme.ls03};
   }
 `;
