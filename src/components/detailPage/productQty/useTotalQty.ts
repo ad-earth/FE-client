07a93986a732664cddf3ee195e0c66dcc76dd0d3
Haesol <osunpine@gmail.com>
2022-11-05 09:04:14 +0900

    📂 docs. 파일명 변경

--- src/components/detailPage/productQty/useTotalQty.ts
+++
@@ -1,7 +0,0 @@
-import { OptionListType } from "../../../shared/types/types";
-
-export const useTotalQty = (optionList: OptionListType) => {
-  let totalQty = 0;
-  optionList.map((option) => (totalQty += option.qty));
-  return totalQty;
-};
