64c74f634a06db5ee18399669b83de44fb742fb5
Haesol <osunpine@gmail.com>
2023-01-27 15:28:32 +0900

    🔍 refactor. 코드 정리

--- src/shared/utils/putCartDB.ts
+++ src/shared/utils/putCartDB.ts
@@ -11,30 +11,30 @@ export const putCartDB = async (
   totalOptionQty: number,
   totalPrice: number
 ) => {
-  let store;
   const db = await openDB("cart", 1, {
     upgrade(db) {
-      store = db.createObjectStore("cart", {
+      db.createObjectStore("cart", {
         keyPath: "id",
         autoIncrement: true,
       });
     },
   });
-  store = db.transaction("cart", "readwrite").objectStore("cart");
-  store.put({
-    id: detailData?.product.p_No,
-    keywordNo: detailData?.k_No,
-    productNo: detailData?.product.p_No,
-    thumbnail: detailData?.product.p_Thumbnail,
-    category: detailData?.product.p_Category,
-    brand: detailData?.product.a_Brand,
-    name: detailData?.product.p_Name,
-    price: detailData?.product.p_Cost,
-    discount: detailData?.product.p_Discount,
-    option: optionList,
-    totalPrice: totalOptionPrice !== 0 ? totalOptionPrice : totalPrice,
-    totalQty: totalOptionQty !== 0 ? totalOptionQty : qty,
-  });
+  db.transaction("cart", "readwrite")
+    .objectStore("cart")
+    .put({
+      id: detailData?.product.p_No,
+      keywordNo: detailData?.k_No,
+      productNo: detailData?.product.p_No,
+      thumbnail: detailData?.product.p_Thumbnail,
+      category: detailData?.product.p_Category,
+      brand: detailData?.product.a_Brand,
+      name: detailData?.product.p_Name,
+      price: detailData?.product.p_Cost,
+      discount: detailData?.product.p_Discount,
+      option: optionList,
+      totalPrice: totalOptionPrice !== 0 ? totalOptionPrice : totalPrice,
+      totalQty: totalOptionQty !== 0 ? totalOptionQty : qty,
+    });
 };
 
 export const putAllCartDB = async (data: CartResponseType) => {
