43d0583ef4474641319a053d951e29a10dd2ce00
sol <osunpine@gmail.com>
2023-02-26 01:27:14 +0900

    🐛 fix. 리스트 모달 삭제

--- src/components/listPage/cardIcon/CardIcon.tsx
+++ src/components/listPage/cardIcon/CardIcon.tsx
@@ -4,24 +4,14 @@ import { useQueryClient } from "react-query";
 import { useNavigate, useParams } from "react-router-dom";
 import { useIcon } from "./useCardIconQuery";
 import { ProductsType } from "../../../containers/listPage/cardList/cardList.type";
-import ListModal from "../../modal/listModal/ListModal";
-import { useGetDetailQuery } from "../../../containers/detailPage/details/useGetDetailQuery";
 
-const CardIcon = ({
-  val,
-  userLike,
-}: {
-  val: ProductsType;
-  userLike: number[];
-}) => {
+const CardIcon = ({ val, userLike }: { val: ProductsType; userLike: number[] }) => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { mutate } = useIcon();
   const { category } = useParams<{ category: string }>();
   const { keyParams } = useParams<{ keyParams: string }>();
   const [plus, setPlus] = useState<number>(val.p_Like);
-  const [productNo, setProductNo] = useState<number>(null);
-  const [infoIsOpen, setInfoIsOpen] = useState<boolean>(false);
   const [include, setInculde] = useState<boolean>(userLike.includes(val.p_No));
 
   //--찜하기 버튼
@@ -36,16 +26,6 @@ const CardIcon = ({
         alert("로그인 후 사용가능합니다.");
       },
     });
-  };
-
-  //-- 리스트 모달
-  const { refetch } = useGetDetailQuery(
-    productNo ? String(productNo) : null,
-    keyParams !== undefined ? keyParams : null
-  );
-  const ModalClick = () => {
-    setInfoIsOpen(true);
-    setProductNo(val.p_No);
   };
 
   const MessageClick = () => {
@@ -64,20 +44,9 @@ const CardIcon = ({
             <t.Count>{val.p_Review}</t.Count>
           </t.IconSpan>
           <t.IconSpan>
-            {include ? (
-              <t.HeartIcon onClick={heartClick} />
-            ) : (
-              <t.EmptyHeartIcon onClick={heartClick} />
-            )}
+            {include ? <t.HeartIcon onClick={heartClick} /> : <t.EmptyHeartIcon onClick={heartClick} />}
             <t.Count>{plus}</t.Count>
           </t.IconSpan>
-          <t.CartIcon onClick={ModalClick} />
-          <t.ModalDiv>
-            <ListModal
-              isOpen={infoIsOpen}
-              handleClose={() => setInfoIsOpen(false)}
-            />
-          </t.ModalDiv>
         </t.IconDiv>
       </t.CardCp>
     </>
